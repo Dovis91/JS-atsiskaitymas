f23f8e6b670ec8b87fe58ebb98b195ecf706ce86
Dovis91 <dovydas.ziukas@gmail.com>
2022-03-22 12:46:23 +0200

    Aštunta užduotis

--- JS atsiskaitymas/Task 8/script.js
+++ JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,39 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor() {}
+  sum = (sk1, sk2) => {
+    let sudetis = sk1 + sk2;
+    return sudetis;
+  };
+  subtraction = (sk1, sk2) => {
+    let atimtis = sk1 - sk2;
+    return atimtis;
+  };
+  multiplication = (sk1, sk2) => {
+    let daugyba = sk1 * sk2;
+    return daugyba;
+  };
+  division = (sk1, sk2) => {
+    let dalyba = sk1 / sk2;
+    return dalyba;
+  };
+}
+
+// Metodas: sudėtis
+let sudetis = new Calculator();
+console.log(sudetis.sum(5, 5));
+
+// Metodas: atimtis
+let atimtis = new Calculator();
+console.log(atimtis.subtraction(20, 15));
+
+// Metodas: daugyba
+let daugyba = new Calculator();
+console.log(daugyba.multiplication(10, 10));
+
+// Metodas: dalyba
+let dalyba = new Calculator();
+console.log(dalyba.division(100, 5));
