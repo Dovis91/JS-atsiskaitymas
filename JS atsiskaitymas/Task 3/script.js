1ab92f7d4f7a27228951aa9f73b46e49509a19a5
Dovis91 <dovydas.ziukas@gmail.com>
2022-03-22 12:43:03 +0200

    Trečia užduotis

--- JS atsiskaitymas/Task 3/script.js
+++ JS atsiskaitymas/Task 3/script.js
@@ -10,4 +10,23 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+let button = document.querySelector("#btn");
+button.addEventListener("click", () => {
+  document.querySelector("#message").classList.add("slepti");
+
+  fetch(ENDPOINT)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        document.querySelector("#output").innerHTML += `
+      <div class="mainDiv"> 
+        <h2 class="userClass"> User login: ${element.login} </h2>
+        <img class="imgClass" src="${element.avatar_url}" width="200px">
+      </div>
+      `;
+      });
+    });
+});
