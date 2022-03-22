1fb0cf907c68ae3e7ee952caf0022bf74765f36e
Dovis91 <dovydas.ziukas@gmail.com>
2022-03-22 12:44:35 +0200

    Septinta užduotis

--- JS atsiskaitymas/Task 7/script.js
+++ JS atsiskaitymas/Task 7/script.js
@@ -5,8 +5,13 @@ Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  marke: 'audi',
-  model: 'A6',
+  marke: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white'
+  color: "white",
 };
+
+let showObjectKeys = (objectas) => {
+  return Object.keys(objectas);
+};
+console.log(showObjectKeys(audi));
